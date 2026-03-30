1aacce1e2ced93ad66485d25d480bd6aa7901482
En-Nya-prog <sieenockyaw@gmail.com>
2026-03-30 11:15:12 +0000

    Use Railway public URL

--- config.py
+++ config.py
@@ -7,15 +7,10 @@ class Config:
     WTF_CSRF_ENABLED = True
     WTF_CSRF_TIME_LIMIT = 3600
 
-    MYSQL_HOST     = os.environ.get("MYSQL_HOST", "localhost")
-    MYSQL_USER     = os.environ.get("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
-    MYSQL_DB       = os.environ.get("MYSQL_DB", "gaming_app_db")
-    MYSQL_PORT     = os.environ.get("MYSQL_PORT", "3306")
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
-        f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "MYSQL_PUBLIC_URL",
+        "mysql+pymysql://root@localhost/gaming_app_db"
+    ).replace("mysql://", "mysql+pymysql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
