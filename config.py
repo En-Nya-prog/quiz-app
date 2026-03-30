9c386b1c5f11a89dc469269a91141c2803b0f0ce
En-Nya-prog <sieenockyaw@gmail.com>
2026-03-30 10:51:02 +0000

    Fix database port in connection string

--- config.py
+++ config.py
@@ -7,15 +7,14 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-change-this")
     WTF_CSRF_ENABLED = True
     WTF_CSRF_TIME_LIMIT = 3600
-
-    MYSQL_HOST     = os.environ.get("MYSQL_HOST", "localhost")
-    MYSQL_USER     = os.environ.get("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
-    MYSQL_DB       = os.environ.get("MYSQL_DB", "gaming_app_db")
-
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
-    )
+MYSQL_HOST     = os.environ.get("MYSQL_HOST", "localhost")
+MYSQL_USER     = os.environ.get("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
+MYSQL_DB       = os.environ.get("MYSQL_DB", "gaming_app_db")
+MYSQL_PORT     = os.environ.get("MYSQL_PORT", "3306")
+SQLALCHEMY_DATABASE_URI = (
+    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
